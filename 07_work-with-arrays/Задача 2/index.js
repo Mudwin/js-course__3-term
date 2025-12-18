0cf1f4c600b33206ce1e98d53951e06665d10a74
Mudwin <nshmilsson@gmail.com>
2025-12-19 00:54:24 +0500

    + 7th module (2/3)

---
+++ 07_work-with-arrays/Задача 2/index.js
@@ -0,0 +1,59 @@
+const addHeightButtonElement = document.querySelector(".add-height");
+const sortHeightButtonElement = document.querySelector(".sort-height");
+const listElement = document.querySelector(".list");
+
+const heights = [164, 157, 160, 143, 170];
+
+const handleAddHeightButtonClick = () => {
+  let newHeight = prompt("Введите рост ученика");
+
+  if (
+    newHeight.trim() == "" ||
+    isNaN(Number(newHeight.trim())) ||
+    newHeight <= 0
+  ) {
+    alert("Рост не введён!");
+  }
+
+  heights.push(newHeight);
+  refreshDOM();
+};
+
+const handleSortHeightButtonClick = () => {
+  let minimumHeight = prompt("Введите минимальный рост");
+
+  if (minimumHeight.trim() == "" || isNaN(Number(minimumHeight.trim()))) {
+    refreshDOM();
+  }
+
+  listElement.innerHTML = "";
+  let count = 1;
+
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] < minimumHeight) {
+      continue;
+    }
+
+    let listItem = document.createElement("li");
+    listItem.textContent = `${count}) ${heights[i]}`;
+    count += 1;
+    listElement.append(listItem);
+  }
+};
+
+const refreshDOM = () => {
+  listElement.innerHTML = "";
+
+  for (let i = 0; i < heights.length; i++) {
+    let listItem = document.createElement("li");
+    listItem.textContent = `${i + 1}) ${heights[i]}`;
+    listElement.append(listItem);
+  }
+};
+
+refreshDOM();
+
+addHeightButtonElement.addEventListener("click", handleAddHeightButtonClick);
+sortHeightButtonElement.addEventListener("click", handleSortHeightButtonClick);
+
+console.log();
